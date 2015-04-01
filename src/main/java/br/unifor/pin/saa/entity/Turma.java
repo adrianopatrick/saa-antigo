1839461c102489d102e7c0c1fa9336ee43f0bc72
MarceloRosa <marcelorosa2@hotmail.com>
2015-03-31 21:25:35 -0300

    alteração do head na entity Turma

--- src/main/java/br/unifor/pin/saa/entity/Turma.java
+++ src/main/java/br/unifor/pin/saa/entity/Turma.java
@@ -16,7 +16,9 @@ import javax.persistence.Table;
 @Table(name="turma")
 public class Turma implements Serializable {
 
+
 	private static final long serialVersionUID = -1203019308743583974L;
+
 
 	@Id
 	@SequenceGenerator(name="turma_seq", sequenceName="turma_seq", allocationSize=1)
