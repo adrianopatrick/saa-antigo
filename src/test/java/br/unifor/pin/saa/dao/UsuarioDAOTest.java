4adf3582ab41805822d6f8f8ed04f0e10e5082e6
MarceloRosa <marcelorosa2@hotmail.com>
2015-04-11 01:09:00 -0300

    Classe UsuariosDAOTest para correção

--- src/test/java/br/unifor/pin/saa/dao/UsuarioDAOTest.java
+++ src/test/java/br/unifor/pin/saa/dao/UsuarioDAOTest.java
@@ -1,13 +1,14 @@
 package br.unifor.pin.saa.dao;
 
-import java.util.List;
+import junit.framework.Assert;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.unifor.pin.saa.entity.Usuarios;
 
@@ -17,7 +18,7 @@ public class UsuarioDAOTest {
 	
 	@Autowired
 	private UsuarioDAO usuarioDAO;
-
+	
 	@Test
 	public void testSalvar() throws Exception {
 		
@@ -33,7 +34,54 @@ public class UsuarioDAOTest {
 		System.out.println(usuario.getId());
 		
 	}
-	
+
+	@Test
+	public void testAtualizar() throws Exception  {
+		final String nome = "Adriano";
+		final String nomeAlterado = "PROFESSOR ADRIANO";
+		final String email = "adriano@gmail.com";
+		final String senha = "123456";
+		final boolean primeiroAcesso = (true);
+		final boolean ativo = (true);
+		
+		
+		Usuarios usuario = new Usuarios();
+		usuario.setNome(nome);
+		usuario.setSenha(senha);
+		usuario.setEmail(email);
+		usuario.setPrimeiroAcesso(primeiroAcesso);
+		usuario.setAtivo(ativo);
+		
+		usuarioDAO.salvar(usuario);
+		usuario.setNome(nomeAlterado);
+		usuarioDAO.atualizar(usuario);
+		usuario = usuarioDAO.buscaPorId(usuario.getId());
+		
+		Assert.assertEquals(nomeAlterado, usuario.getNome());
+		
+		
+		
+		usuarioDAO.excluir(usuario);
+		
+	}
+	@Test
+	public void testBuscarPorId() throws Exception {
+		
+		Usuarios usuario = new Usuarios();
+		usuario.setNome("adriano");
+		usuario.setSenha("123456");
+		usuario.setEmail("adriano@gmail.com");
+		usuario.setPrimeiroAcesso(true);
+		usuario.setAtivo(false);
+		usuarioDAO.salvar(usuario);
+
+		//Usuarios usuarioRetorno = Usua......
+		//Long id = usuarioRetorno.get
+		Assert.assertNotNull(usuario.getId());
+		System.out.println(usuario.getId());
+		
+	}
+		
 	@Test
 	public void testListaPorNome(){
 		List<Usuarios> usuarios = usuarioDAO.listarPorNome("adri");
