7738b57f70d9c373b1883e17c3dddf1c5fa85282
MarceloRosa <marcelorosa2@hotmail.com>
2015-05-08 18:12:28 -0300

    Criação TurmaBO

---
+++ src/main/java/br/unifor/pin/saa/bussines/TurmaBO.java
@@ -0,0 +1,61 @@
+package br.unifor.pin.saa.bussines;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.unifor.pin.saa.dao.TurmaDAO;
+import br.unifor.pin.saa.entity.Turma;
+
+public class TurmaBO {
+	
+	private static final Logger logger = LoggerFactory
+			.getLogger(TurmaBO.class);
+
+	@Autowired
+	private TurmaDAO turmaDAO;
+	
+	public void salvar(Turma turma) {
+		loggerInit("salvar");
+		turmaDAO.salvar(turma);
+		loggerFinhish("salvar");
+	}
+	
+	public void atualizar(Turma turma){
+		loggerInit("atualizar");
+		turmaDAO.atualizar(turma);
+		loggerFinhish("atualizar");
+		
+	}
+	
+	public Turma buscarPorId(Long id){
+		return turmaDAO.buscarPorId(id);
+	}
+	
+	@Transactional(propagation=Propagation.REQUIRES_NEW)
+	public void excluir(Turma turma) {
+		loggerInit("excluir");
+		turma = turmaDAO.buscarPorId(turma.getId());
+		turmaDAO.excluir(turma);
+		loggerFinhish("excluir");
+	}
+
+	public void loggerInit(String method) {
+		logger.debug("Inicio do método " + method + " da classe"
+				+ this.getClass().getName());
+	}
+
+	public void loggerFinhish(String method) {
+		logger.debug("Fim do método "+method+" da classe"
+				+ this.getClass().getName());
+	}
+	
+	
+	
+	
+}
+
+
+
