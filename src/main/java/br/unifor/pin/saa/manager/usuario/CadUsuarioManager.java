f848a9c9bb165ae0cbefa44ebfe83075bd926111
jorge <jorge@jorge-Inspiron-3442>
2015-06-18 11:26:04 -0300

    Adicionado menu lateral

--- src/main/java/br/unifor/pin/saa/manager/usuario/CadUsuarioManager.java
+++ src/main/java/br/unifor/pin/saa/manager/usuario/CadUsuarioManager.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import br.unifor.pin.saa.bussines.UsuarioBO;
+import br.unifor.pin.saa.entity.TipoUsuario;
 import br.unifor.pin.saa.entity.Usuarios;
 import br.unifor.pin.saa.utils.MessagesUtils;
 import br.unifor.pin.saa.utils.Navigation;
@@ -23,12 +24,17 @@ public class CadUsuarioManager {
 	private String nome;
 	private String email;
 	private String senha;
+	private Integer tipo;
+	private TipoUsuario tipoUsuario;
+	private String vinculo;
 	
 	public String salvar(){
 		Usuarios usuario = new Usuarios();
 		usuario.setNome(nome);
 		usuario.setEmail(email);
 		usuario.setSenha(senha);
+		usuario.setTipoUsuario(tipoUsuario);
+		
 		usuarioBO.salvar(usuario);
 		MessagesUtils.info("Usuário salvo com sucesso!");
 		listUsuario.lista();
@@ -47,7 +53,7 @@ public class CadUsuarioManager {
 		this.email = "";
 		this.senha = "";
 	}
-
+	
 	public String getNome() {
 		return nome;
 	}
@@ -67,6 +73,36 @@ public class CadUsuarioManager {
 	}
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+
+	public Integer getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(Integer tipo) {
+		this.tipo = tipo;
+	}
+
+	public TipoUsuario getTipoUsuario() {
+		return tipoUsuario;
+	}
+
+	public void setTipoUsuario(TipoUsuario tipoUsuario) {
+		//this.tipoUsuario = tipoUsuario;
+		if (vinculo == "professor"){
+			setTipoUsuario(tipoUsuario.PROFESSOR);
+		}
+		else if (vinculo == "aluno"){
+			setTipoUsuario(tipoUsuario.ALUNO);
+		}
+	}
+
+	public String getVinculo() {
+		return vinculo;
+	}
+
+	public void setVinculo(String vinculo) {
+		this.vinculo = vinculo;
 	}
 	
 }
